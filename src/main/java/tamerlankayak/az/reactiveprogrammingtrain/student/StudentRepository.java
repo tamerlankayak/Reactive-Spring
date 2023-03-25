25706266434a368bf69a1c46d1add1daaa3670cd
Tamerlan Mustafayev <tamerlan_94@mail.ru>
2023-03-25 20:34:22 +0400

    Update StudentRepository.java

--- src/main/java/tamerlankayak/az/reactiveprogrammingtrain/student/StudentRepository.java
+++ src/main/java/tamerlankayak/az/reactiveprogrammingtrain/student/StudentRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 
 public interface StudentRepository extends ReactiveCrudRepository<Student, Integer> {
-
+    //for fetching data list we are usinng Flux class
     Flux<Student> findAllByFirstnameIgnoreCase(String firstname);
 }
